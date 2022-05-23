944f2b36595c41a4bd36c412cb3e773c85721d6a
Ubiratan Júnior <r14.ubiratanjunior@gmail.com>
2022-05-23 20:46:59 -0300

    Adiciona módulo local de roles

--- main.tf
+++ main.tf
@@ -2,7 +2,12 @@ provider "aws" {
   region = "sa-east-1" # Setando região de São Paulo
 }
 
-#Pasta com criação das roles
+#Módulo com definição das roles
 module "roles" {
   source = "./roles"
+}
+
+# Módulo com definição do bucket usado
+module "buckets" {
+  source = "./buckets"
 }
--- roles/main.tf
+++ roles/main.tf
@@ -1,10 +1,12 @@
+# Criando policy
 resource "aws_iam_policy" "policy" {
   name        = "lambda-apigateway-policy"
-  description = "Política que autoriza acesso ao DynamoDB e Amazon CloudWatch Logs."
+  description = "Política que autoriza acesso ao DynamoDB, Amazon CloudWatch Logs e execução da Lambda function."
 
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
+      # Permissões do banco de dados
       {
         Action = [
           "dynamodb:DeleteItem",
@@ -19,6 +21,7 @@ resource "aws_iam_policy" "policy" {
         Resource = "*"
         Sid      = "Stmt1428341300017"
       },
+      # Permissões do Cloudwatch logs
       {
         Action = [
           "logs:CreateLogGroup",
@@ -29,12 +32,26 @@ resource "aws_iam_policy" "policy" {
         Effect   = "Allow"
         Resource = "*"
         Sid      = ""
+      },
+      # Permissão de execução
+      {
+        Action = [
+          "sts:AssumeRole"
+        ]
+
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        },
+
+        Effect = "Allow"
+        Sid    = ""
       }
     ]
   })
 }
 
+# Vinculando policy com role
 resource "aws_iam_role" "role" {
   name               = "lambda-apigateway-role"
-  assume_role_policy = aws_iam_policy.policy.arn
+  assume_role_policy = aws_iam_policy.policy.id
 }

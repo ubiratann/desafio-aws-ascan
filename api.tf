b2c7a2f07f084dae65f4261d335316d3b3cd968f
Ubiratan Júnior <r14.ubiratanjunior@gmail.com>
2022-05-28 14:11:36 -0300

    Atualiza resource para padrao apigatewayv2

--- api.tf
+++ api.tf
@@ -13,29 +13,33 @@ resource "aws_apigatewayv2_integration" "integration" {
   api_id                 = aws_apigatewayv2_api.this.id
   integration_type       = "AWS_PROXY"
   integration_method     = "POST"
-  integration_uri        = aws_lambda_function.todos[each.key].invoke_arn
+  integration_uri        = aws_lambda_function.todos.invoke_arn
   payload_format_version = "2.0"
 }
 
-resource "aws_apigatewayv2_route" "put" {
-  for_each = local.lambdas
-
+resource "aws_apigatewayv2_route" "delete" {
   api_id    = aws_apigatewayv2_api.api.id
-  route_key = "${upper(each.key)} /v1/todos"
-  target    = "integrations/${aws_apigatewayv2_integration.todos[each.key].id}"
+  route_key = "DELETE /v1/todos"
+  target    = "integrations/${aws_apigatewayv2_integration.integration.id}"
 }
+
 
 resource "aws_apigatewayv2_route" "post" {
-
+  api_id    = aws_apigatewayv2_api.api.id
+  route_key = "POST /v1/todos"
+  target    = "integrations/${aws_apigatewayv2_integration.integration.id}"
 }
 
-resource "aws_apigatewayv2_route" "delete" {
-
+resource "aws_apigatewayv2_route" "put" {
+  api_id    = aws_apigatewayv2_api.api.id
+  route_key = "PUT /v1/todos"
+  target    = "integrations/${aws_apigatewayv2_integration.integration.id}"
 }
+
 
 resource "aws_apigatewayv2_route" "get" {
   api_id    = aws_apigatewayv2_api.this.id
-  route_key = "GET /v1/todos/{todoId}"
-  target    = "integrations/${aws_apigatewayv2_integration.todos["get"].id}"
+  route_key = "GET /v1/todos/{id}"
+  target    = "integrations/${aws_apigatewayv2_integration.integration.id}"
 }
 

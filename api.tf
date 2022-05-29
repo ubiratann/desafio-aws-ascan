6ddfec6360cc905db34746902725e312fa414117
Ubiratan Júnior <r14.ubiratanjunior@gmail.com>
2022-05-29 20:11:03 -0300

    Corrige uri de integração

--- api.tf
+++ api.tf
@@ -13,7 +13,7 @@ resource "aws_apigatewayv2_integration" "integration" {
   api_id                 = aws_apigatewayv2_api.this.id
   integration_type       = "AWS_PROXY"
   integration_method     = "POST"
-  integration_uri        = aws_lambda_function.todos.invoke_arn
+  integration_uri        = aws_lambda_function.lambda.invoke_arn
   payload_format_version = "2.0"
 }
 

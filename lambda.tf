6a1452adde37843b3e3691d497bfbfa6579f90d1
Ubiratan Júnior <r14.ubiratanjunior@gmail.com>
2022-05-29 20:08:46 -0300

    Altera nome da lambda e adiciona permissão

--- lambda.tf
+++ lambda.tf
@@ -1,13 +1,22 @@
 # Criando lambda
 resource "aws_lambda_function" "lambda" {
+
   filename      = "lambda.zip"
-  function_name = "TodoAppLambda"
+  function_name = "todo-list-lambda"
   handler       = "main.handler"
   runtime       = "python3.8"
   role          = aws_iam_role.role.arn
+
   environment {
     variables = {
-      "TODO_LIST_TABLE" = "todo-app"
+      "TABLE" = "todo-list"
     }
   }
+}
+
+resource "aws_lambda_permission" "permission" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.arn
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "arn:aws:execute-api:${var.region}:*:${aws_apigatewayv2_api.api.id}/*"
 }

cf1a91a1800573177d99859d4135a2157eba2c26
Ubiratan Júnior <r14.ubiratanjunior@gmail.com>
2022-05-29 20:09:51 -0300

    Remove uso de módulos

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
 provider "aws" {
-  region = "sa-east-1" # Setando região de São Paulo
-}
-
-#Módulo com definição das roles
-module "roles" {
-  source = "./roles"
-}
-
-# Módulo com definição do bucket usado
-module "buckets" {
-  source = "./buckets"
+  region = var.region # Setando região de São Paulo
 }

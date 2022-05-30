af7f2c1f46b7d6b4562d4602fcb8bcbcaad62be6
Ubiratan <r14.ubiratan.junior@gmail.com>
2022-05-30 15:17:36 -0300

    Corrige rota de update e remove rota de get_all

--- api.tf
+++ api.tf
@@ -49,19 +49,13 @@ resource "aws_apigatewayv2_route" "finish_task" {
 
 resource "aws_apigatewayv2_route" "update" {
   api_id    = aws_apigatewayv2_api.api.id
-  route_key = "PUT /v1//update/{id}"
+  route_key = "PUT /v1/todo/update/{id}"
   target    = "integrations/${aws_apigatewayv2_integration.integration.id}"
 }
 
 resource "aws_apigatewayv2_route" "get" {
   api_id    = aws_apigatewayv2_api.api.id
   route_key = "GET /v1/todo/{id}"
-  target    = "integrations/${aws_apigatewayv2_integration.integration.id}"
-}
-
-resource "aws_apigatewayv2_route" "get_all" {
-  api_id    = aws_apigatewayv2_api.api.id
-  route_key = "GET /v1/todo/all"
   target    = "integrations/${aws_apigatewayv2_integration.integration.id}"
 }
 

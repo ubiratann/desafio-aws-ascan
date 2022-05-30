6a68c96ab2c1170c05d94a7f127461b888fbc686
Ubiratan <r14.ubiratan.junior@gmail.com>
2022-05-30 14:52:03 -0300

    Atualiza string das rotas

--- api.tf
+++ api.tf
@@ -19,48 +19,48 @@ resource "aws_apigatewayv2_integration" "integration" {
 
 resource "aws_apigatewayv2_route" "delete" {
   api_id    = aws_apigatewayv2_api.api.id
-  route_key = "DELETE /v1/todos/{id}"
+  route_key = "DELETE /v1/todo/{id}"
   target    = "integrations/${aws_apigatewayv2_integration.integration.id}"
 }
 
 resource "aws_apigatewayv2_route" "post" {
   api_id    = aws_apigatewayv2_api.api.id
-  route_key = "POST /v1/todos"
+  route_key = "POST /v1/todo"
   target    = "integrations/${aws_apigatewayv2_integration.integration.id}"
 }
 
 resource "aws_apigatewayv2_route" "start_task" {
   api_id    = aws_apigatewayv2_api.api.id
-  route_key = "PUT /v1/todos/start/{id}"
+  route_key = "PUT /v1/todo/start/{id}"
   target    = "integrations/${aws_apigatewayv2_integration.integration.id}"
 }
 
 resource "aws_apigatewayv2_route" "stop_task" {
   api_id    = aws_apigatewayv2_api.api.id
-  route_key = "PUT /v1/todos/stop/{id}"
+  route_key = "PUT /v1/todo/stop/{id}"
   target    = "integrations/${aws_apigatewayv2_integration.integration.id}"
 }
 
 resource "aws_apigatewayv2_route" "finish_task" {
   api_id    = aws_apigatewayv2_api.api.id
-  route_key = "PUT /v1/todos/finish/{id}"
+  route_key = "PUT /v1/todo/finish/{id}"
   target    = "integrations/${aws_apigatewayv2_integration.integration.id}"
 }
 
 resource "aws_apigatewayv2_route" "update" {
   api_id    = aws_apigatewayv2_api.api.id
-  route_key = "PUT /v1/todos/update/{id}"
+  route_key = "PUT /v1//update/{id}"
   target    = "integrations/${aws_apigatewayv2_integration.integration.id}"
 }
 
 resource "aws_apigatewayv2_route" "get" {
   api_id    = aws_apigatewayv2_api.api.id
-  route_key = "GET /v1/todos/{id}"
+  route_key = "GET /v1/todo/{id}"
   target    = "integrations/${aws_apigatewayv2_integration.integration.id}"
 }
 
 resource "aws_apigatewayv2_route" "get_all" {
   api_id    = aws_apigatewayv2_api.api.id
-  route_key = "GET /v1/todos/}"
+  route_key = "GET /v1/todo/}"
   target    = "integrations/${aws_apigatewayv2_integration.integration.id}"
 }

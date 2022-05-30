3aa932dcb9ae1b37b8295c4b847a2a9b2714c738
Ubiratan Júnior <r14.ubiratanjunior@gmail.com>
2022-05-30 03:00:56 -0300

    Refatora e adiciona rotas

--- api.tf
+++ api.tf
@@ -19,10 +19,9 @@ resource "aws_apigatewayv2_integration" "integration" {
 
 resource "aws_apigatewayv2_route" "delete" {
   api_id    = aws_apigatewayv2_api.api.id
-  route_key = "DELETE /v1/todos"
+  route_key = "DELETE /v1/todos/{id}"
   target    = "integrations/${aws_apigatewayv2_integration.integration.id}"
 }
-
 
 resource "aws_apigatewayv2_route" "post" {
   api_id    = aws_apigatewayv2_api.api.id
@@ -30,12 +29,29 @@ resource "aws_apigatewayv2_route" "post" {
   target    = "integrations/${aws_apigatewayv2_integration.integration.id}"
 }
 
-resource "aws_apigatewayv2_route" "put" {
+resource "aws_apigatewayv2_route" "start_task" {
   api_id    = aws_apigatewayv2_api.api.id
-  route_key = "PUT /v1/todos"
+  route_key = "PUT /v1/todos/start/{id}"
   target    = "integrations/${aws_apigatewayv2_integration.integration.id}"
 }
 
+resource "aws_apigatewayv2_route" "stop_task" {
+  api_id    = aws_apigatewayv2_api.api.id
+  route_key = "PUT /v1/todos/stop/{id}"
+  target    = "integrations/${aws_apigatewayv2_integration.integration.id}"
+}
+
+resource "aws_apigatewayv2_route" "finish_task" {
+  api_id    = aws_apigatewayv2_api.api.id
+  route_key = "PUT /v1/todos/finish/{id}"
+  target    = "integrations/${aws_apigatewayv2_integration.integration.id}"
+}
+
+resource "aws_apigatewayv2_route" "update" {
+  api_id    = aws_apigatewayv2_api.api.id
+  route_key = "PUT /v1/todos/update/{id}"
+  target    = "integrations/${aws_apigatewayv2_integration.integration.id}"
+}
 
 resource "aws_apigatewayv2_route" "get" {
   api_id    = aws_apigatewayv2_api.api.id
@@ -43,3 +59,8 @@ resource "aws_apigatewayv2_route" "get" {
   target    = "integrations/${aws_apigatewayv2_integration.integration.id}"
 }
 
+resource "aws_apigatewayv2_route" "get_all" {
+  api_id    = aws_apigatewayv2_api.api.id
+  route_key = "GET /v1/todos/}"
+  target    = "integrations/${aws_apigatewayv2_integration.integration.id}"
+}

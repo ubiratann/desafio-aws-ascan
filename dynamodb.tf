dbe32d719bcdc2b3d61986964d6c0c87f35c3f7a
Ubiratan Júnior <r14.ubiratanjunior@gmail.com>
2022-05-30 03:02:05 -0300

    Simplifica estrutura do banco

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,34 +1,11 @@
 resource "aws_dynamodb_table" "todo" {
-  billing_mode   = "PROVISIONED"
-  hash_key       = "id"
-  name           = "todo-list"
-  read_capacity  = 20
-  write_capacity = 20
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+  name         = "todo-list"
 
   attribute {
-
     name = "id"
     type = "N"
-  }
-
-  attribute {
-    name = "description"
-    type = "S"
-  }
-
-  attribute {
-    name = "status"
-    type = "N"
-  }
-
-  global_secondary_index {
-    name               = "descriptionIndex"
-    hash_key           = "description"
-    range_key          = "status"
-    write_capacity     = 10
-    read_capacity      = 10
-    projection_type    = "INCLUDE"
-    non_key_attributes = ["id"]
   }
 }
 

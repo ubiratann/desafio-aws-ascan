be4e0190fdcdf8acf4861be7f0de6ff424b88e85
Ubiratan Júnior <r14.ubiratanjunior@gmail.com>
2022-05-29 21:00:41 -0300

    Refatora estrutra de políticas e roles

--- iam.tf
+++ iam.tf
@@ -1,57 +1,46 @@
-# Criando policy
-resource "aws_iam_policy" "policy" {
-  name        = "lambda-apigateway-policy"
-  description = "Política que autoriza acesso ao DynamoDB, Amazon CloudWatch Logs e execução da Lambda function."
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      # Permissões do banco de dados
-      {
-        Action = [
-          "dynamodb:DeleteItem",
-          "dynamodb:GetItem",
-          "dynamodb:PutItem",
-          "dynamodb:Query",
-          "dynamodb:Scan",
-          "dynamodb:UpdateItem"
-        ]
-
-        Effect   = "Allow"
-        Resource = "*"
-        Sid      = "Stmt1428341300017"
-      },
-      # Permissões do Cloudwatch logs
-      {
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ]
-
-        Effect   = "Allow"
-        Resource = "*"
-        Sid      = ""
-      },
-      # Permissão de execução
-      {
-        Action = [
-          "sts:AssumeRole"
-        ]
-
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        },
-
-        Effect = "Allow"
-        Sid    = ""
-      }
-    ]
-  })
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
 }
 
-# Vinculando policy com role
-resource "aws_iam_role" "role" {
-  name               = "lambda-apigateway-role"
-  assume_role_policy = aws_iam_policy.policy.id
+resource "aws_iam_role" "api_role" {
+  name               = "todo-list-lambda-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "aws_iam_policy_document" "dynamo_policys" {
+  statement {
+    effect    = "Allow"
+    resources = ["arn:aws:dyanmodb:${var.region}:*:table/todo-list"]
+    actions = [
+      "dynamodb:PutItem",
+      "dynamodb:DescribeTable",
+      "dynamodb:DeleteItem",
+      "dynamodb:GetItem",
+      "dynamodb:Scan",
+      "dynamodb:Query",
+      "dynamodb:UpdateItem"
+    ]
+  }
+
+  statement {
+    effect    = "Allow"
+    resources = ["*"]
+    actions   = ["dynamodbListTables"]
+  }
+}
+
+resource "aws_iam_policy" "policy" {
+  name   = "todo-list-policy"
+  policy = data.aws_iam_policy_document.dynamo_policys.json
+}
+
+resource "aws_iam_role_policy_attachment" "attachment" {
+  policy_arn = aws_iam_policy.policy.arn
+  role       = aws_iam_role.api_role.name
 }

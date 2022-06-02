9b98243c81a5dbb5b9f26e8b655bbcde9153960a
Ubiratan Júnior <r14.ubiratanjunior@gmail.com>
2022-06-02 02:48:03 -0300

    Adiciona tag que proibe abrir o pager do aws cli

--- update_lambda.sh
+++ update_lambda.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 zip -rj  lambda.zip src/* 
-aws lambda update-function-code --function-name "todo-list" --zip-file "fileb://./lambda.zip" --region "sa-east-1"
+aws lambda update-function-code --function-name "todo-list" --zip-file "fileb://./lambda.zip" --region "sa-east-1" --no-cli-pager

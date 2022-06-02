c5e07f76647dc4606dc6efea2451edcd973e3de1
Ubiratan Júnior <r14.ubiratanjunior@gmail.com>
2022-06-02 02:44:16 -0300

    Adiciona estrutura de indice secundário

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,11 +1,31 @@
 resource "aws_dynamodb_table" "todo" {
   billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "id"
   name         = "todo-list"
 
   attribute {
     name = "id"
     type = "N"
+  }
+
+  attribute {
+    name = "status_code"
+    type = "N"
+  }
+
+  attribute {
+    name = "description"
+    type = "S"
+  }
+
+
+  global_secondary_index {
+    name               = "status_code_index"
+    hash_key           = "status_code"
+    range_key          = "description"
+    write_capacity     = 10
+    read_capacity      = 10
+    projection_type    = "INCLUDE"
+    non_key_attributes = ["id"]
   }
 }
 

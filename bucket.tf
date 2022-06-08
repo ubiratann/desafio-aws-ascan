a73fb7972a093fbe1ffd86f95d506b1959fd2819
Ubiratan Júnior <r14.ubiratanjunior@gmail.com>
2022-06-08 20:37:07 -0300

    Cria bucket para versionamento do tfstate

---
+++ bucket.tf
@@ -0,0 +1,9 @@
+data "aws_caller_identity" "current" {}
+
+resource "aws_s3_bucket" "remote-state" {
+  bucket = "tfstate-${data.aws_caller_identity.current.account_id}"
+
+  versioning {
+    enabled = true
+  }
+}

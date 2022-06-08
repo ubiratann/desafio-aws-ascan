c4de77ad6efe84812a717057d6d3e738e25e7b92
Ubiratan Júnior <r14.ubiratanjunior@gmail.com>
2022-06-08 20:37:23 -0300

    Adiciona backend de versionamento do tfstate

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
 provider "aws" {
   region = var.region # Setando região de São Paulo
 }
+
+terraform {
+  backend "s3" {
+    bucket = "tfstate-050485243139"
+    region = "sa-east-1"
+    key    = "terraform.tfstate"
+  }
+}

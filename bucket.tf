1e20b672beb6a7f5ffb23f09ee257a1104ef8961
Ubiratan Júnior <r14.ubiratanjunior@gmail.com>
2022-06-17 13:31:10 -0300

    Remove argumento de versionamento deprecado

--- bucket.tf
+++ bucket.tf
@@ -3,7 +3,12 @@ data "aws_caller_identity" "current" {}
 resource "aws_s3_bucket" "remote-state" {
   bucket = "tfstate-${data.aws_caller_identity.current.account_id}"
 
-  versioning {
-    enabled = true
+}
+
+resource "aws_s3_bucket_versioning" "remote-state-versioning" {
+  bucket = aws_s3_bucket.remote-state.id
+
+  versioning_configuration {
+    status = "Enabled"
   }
 }
